1f255e667cf9195170b02cbcc002a8409091de32
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-04-20 15:41:31 +0200

    Update js by removing reiteration in the for-loop iin the changePizzaSizes method

--- views/js/main.js
+++ views/js/main.js
@@ -455,12 +455,15 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      //Moved coce out of the for-loop
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      var pizzaCount = document.querySelectorAll(".randomPizzaContainer").length;
+
+      for (var i = 0; i < pizzaCount; i++) {
       //Changed from the slower document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-      document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
-    }
+        document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
+      }
   }
 
   changePizzaSizes(size);
@@ -474,19 +477,12 @@ var resizePizzas = function(size) {
 
 window.performance.mark("mark_start_generating"); // collect timing data
 
-// This for-loop actually creates and appends all of the pizzas when the page loads
-// for (var i = 2; i < 100; i++) {
-//   var pizzasDiv = document.getElementById("randomPizzas");
-//   pizzasDiv.appendChild(pizzaElementGenerator(i));
-// }
 
 // Eliminating the need for retrieving same element over and over again.
-
 var pizzasDiv = document.getElementById("randomPizzas");
 for (var i = 2; i < 100; i++) {
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
-
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
 window.performance.mark("mark_end_generating");
